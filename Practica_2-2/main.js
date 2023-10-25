b9e2a09821a96cc6d99ca0d37741e0153e7c5d54
Israeljuarezjimenez <jimenezjuarez.israel@gmail.com>
2023-10-24 21:30:57 -0600

    Este jala mejor xD

--- Practica_2-2/main.js
+++ Practica_2-2/main.js
@@ -1,189 +1,138 @@
 class Juego {
     constructor() {
-        this.cuadricula = document.getElementById("cuadricula");
-        this.botonInicio = document.getElementById("boton-inicio");
-        this.contadorSpan = document.getElementById("contador");
-        this.temporizadorSpan = document.getElementById("temporizador");
-
-        this.rompecabezas = [];
-        this.botonVacio = null;
-        this.enJuego = false;
-        this.movimientos = 0;
-        this.intervaloTemporizador = null;
-
-        this.botonInicio.addEventListener("click", this.iniciarJuego.bind(this));
-        this.cuadricula.addEventListener("click", (e) => this.manipularBoton(e));
-
-        this.inicializar();
+      this.cuadricula = document.getElementById("cuadricula");
+      this.botonInicio = document.getElementById("boton-inicio");
+      this.contadorSpan = document.getElementById("contador");
+      this.temporizadorSpan = document.getElementById("temporizador");
+  
+      this.rompecabezas = [];
+      this.botonVacio = null;
+      this.enJuego = false;
+      this.movimientos = 0;
+      this.intervaloTemporizador = null;
+  
+      this.botonInicio.addEventListener("click", this.iniciarJuego.bind(this));
+  
+      this.inicializar();
     }
-
+  
     inicializar() {
-        this.construirRompecabezas();
-        this.mezclarRompecabezas();
+      this.construirRompecabezas();
     }
-
+  
     construirRompecabezas() {
-        const numRows = 4;
-        const numCols = 4;
-        this.rompecabezas = new Array(numRows);
-
-        for (let i = 0; i< numRows; i++)
-        {
-            this.rompecabezas[i] = new Array (numCols);
-            for(let j = 0; j < numCols; j++)
-            {
-                const button = document.createElement("button");
-                button.className = "numero";
-                button.textContent = i * numCols + j + 1;
-                this.cuadricula.appendChild(button);
-                this.rompecabezas[i][j] = button;
-                button.addEventListener("click", (event) => this.manipularBoton(event));
-            }
+      const numRows = 4;
+      const numCols = 4;
+      this.rompecabezas = new Array(numRows);
+  
+      for (let i = 0; i < numRows; i++) {
+        this.rompecabezas[i] = new Array(numCols);
+        for (let j = 0; j < numCols; j++) {
+          const button = document.createElement("button");
+          button.className = "numero";
+          this.cuadricula.appendChild(button);
+          this.rompecabezas[i][j] = button;
         }
-        this.rompecabezas[numRows -1][numCols -1].textContent = "";
-        this.botonVacio = this.rompecabezas[numRows -1][numCols -1];
+      }
+      this.botonVacio = this.rompecabezas[numRows - 1][numCols - 1];
+      this.rompecabezas[numRows - 1][numCols - 1].textContent = "";
+  
+      this.mezclarRompecabezas();
     }
-
-
+  
     mezclarRompecabezas() {
-        const cantidadMovimientosAleatorios = 100;
-        let ultimoBoton = null;
-    
-        for (let i = 0; i < cantidadMovimientosAleatorios; i++) {
-            const botonesAdyacentes = [];
-    
-            if (ultimoBoton) {
-                const [fila, columna] = this.obtenerPosicion(ultimoBoton);
-    
-                if (this.esAdyacente(this.botonVacio, fila, columna - 1)) {
-                    botonesAdyacentes.push(this.rompecabezas[fila][columna - 1]);
-                }
-                if (this.esAdyacente(this.botonVacio, fila, columna + 1)) {
-                    botonesAdyacentes.push(this.rompecabezas[fila][columna + 1]);
-                }
-                if (this.esAdyacente(this.botonVacio, fila - 1, columna)) {
-                    botonesAdyacentes.push(this.rompecabezas[fila - 1][columna]);
-                }
-                if (this.esAdyacente(this.botonVacio, fila + 1, columna)) {
-                    botonesAdyacentes.push(this.rompecabezas[fila + 1][columna]);
-                }
-            }
-    
-            const botonAleatorio = botonesAdyacentes[Math.floor(Math.random() * botonesAdyacentes.length)];
-            if (botonAleatorio) {
-                this.intercambiarBotones(this.botonVacio, botonAleatorio);
-                ultimoBoton = botonAleatorio;
-            }
+      const numRows = this.rompecabezas.length;
+      const numCols = this.rompecabezas[0].length;
+      const totalCeldas = numRows * numCols;
+      const numerosDesordenados = [...Array(totalCeldas - 1).keys()];
+  
+      for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+          if (this.rompecabezas[i][j] !== this.botonVacio) {
+            const indiceAleatorio = Math.floor(Math.random() * numerosDesordenados.length);
+            const numeroAleatorio = numerosDesordenados.splice(indiceAleatorio, 1)[0];
+            this.rompecabezas[i][j].textContent = numeroAleatorio + 1;
+            this.rompecabezas[i][j].addEventListener("click", (event) => this.manipularBoton(event));
+          }
         }
+      }
     }
-            
-    
-
+  
     intercambiarBotones(boton1, boton2) {
-        const tempGridRow = boton1.style.gridRow;
-        const tempGridColumn = boton1.style.gridColumn;
-    
-        boton1.style.gridRow = boton2.style.gridRow;
-        boton1.style.gridColumn = boton2.style.gridColumn;
-    
-        boton2.style.gridRow = tempGridRow;
-        boton2.style.gridColumn = tempGridColumn;
+      const tempText = boton1.textContent;
+      boton1.textContent = boton2.textContent;
+      boton2.textContent = tempText;
     }
-
-    
+  
     iniciarJuego() {
-        if (!this.enJuego) {
-            this.inicializar();
-            this.botonInicio.disabled = true;
-            this.enJuego = true;
-            let segundos = 0;
-            this.intervaloTemporizador = setInterval(() => {
-                segundos++;
-                const minutos = Math.floor(segundos / 60);
-                const segundosFormateados = String(segundos % 60).padStart(2, "0");
-                this.temporizadorSpan.textContent = `${minutos}:${segundosFormateados}`;
-            }, 1000);
-        }
+      if (!this.enJuego) {
+        this.inicializar();
+        this.botonInicio.disabled = true;
+        this.enJuego = true;
+        let segundos = 0;
+        this.intervaloTemporizador = setInterval(() => {
+          segundos++;
+          const minutos = Math.floor(segundos / 60);
+          const segundosFormateados = String(segundos % 60).padStart(2, "0");
+          this.temporizadorSpan.textContent = `${minutos}:${segundosFormateados}`;
+        }, 1000);
+      }
     }
-
+  
     manipularBoton(evento) {
-        if (this.enJuego) {
-            const boton = evento.target;
-            if (boton.className === "numero") {
-                const [fila, columna] = this.obtenerPosicion(boton);
-    
-                if (this.esAdyacente(this.botonVacio, fila, columna)) {
-                    this.intercambiarBotones(this.botonVacio, boton);
-                    this.movimientos++;
-                    this.contadorSpan.textContent = this.movimientos;
-                    this.botonVacio = boton; // Actualiza el botón vacío
-    
-                    if (this.estaResuelto()) {
-                        clearInterval(this.intervaloTemporizador);
-                        alert("¡Ganaste! ¡Rompecabezas resuelto!");
-                    }
-                }
+      if (this.enJuego) {
+        const boton = evento.target;
+        if (boton.className === "numero") {
+          const botonFila = this.obtenerFila(boton);
+          const botonColumna = this.obtenerColumna(boton);
+          const vacioFila = this.obtenerFila(this.botonVacio);
+          const vacioColumna = this.obtenerColumna(this.botonVacio);
+  
+          if (
+            (Math.abs(botonFila - vacioFila) === 1 && botonColumna === vacioColumna) ||
+            (Math.abs(botonColumna - vacioColumna) === 1 && botonFila === vacioFila)
+          ) {
+            this.intercambiarBotones(boton, this.botonVacio);
+            this.movimientos++;
+            this.contadorSpan.textContent = this.movimientos;
+            this.botonVacio = boton;
+            if (this.estaResuelto()) {
+              clearInterval(this.intervaloTemporizador);
+              alert("¡Ganaste! ¡Rompecabezas resuelto!");
             }
+          }
         }
+      }
     }
-    
-
-    obtenerPosicion(boton) 
-    {
-        for(let i = 0; i < this.rompecabezas.length; i ++)
-        {
-            for(let j = 0; j < this.rompecabezas[i].length; j ++) 
-            {
-                if(this.rompecabezas [i][j] === boton)
-                {
-                    return [i, j];
-                }
-            }
-        }
+  
+    obtenerFila(boton) {
+      return this.rompecabezas.findIndex((fila) => fila.includes(boton));
     }
-
-    esAdyacente(boton1, fila, columna) {
-        if (boton1) {
-            const [botonFila, botonColumna] = this.obtenerPosicion(boton1);
-    
-            const esMismaFila = botonFila === fila;
-            const esMismaColumna = botonColumna === columna;
-    
-            if (esMismaFila && Math.abs(botonColumna - columna) === 1) {
-                return true;
-            }
-    
-            if (esMismaColumna && Math.abs(botonFila - fila) === 1) {
-                return true;
-            }
-        }
-    
-        return false;
+  
+    obtenerColumna(boton) {
+      const fila = this.rompecabezas.find((fila) => fila.includes(boton));
+      return fila ? fila.indexOf(boton) : -1;
     }
-    
-
+  
     estaResuelto() {
-        const numRows = this.rompecabezas.length;
-        const numCols = this.rompecabezas[0].length;
-        let valorEsperado = 1;
-
-        for(let i = 0; i < numRows; i ++)
-        {
-            for(let j = 0; j < numCols; j++)
-            {
-                if(this.rompecabezas[i][j].textContent !== "")
-                {
-                    const valorActual = parseInt (this. rompecabezas[i][j].textContent);
-                    if(valorActual !== valorEsperado)
-                    {
-                        return false;
-                    }
-                    valorEsperado ++;
-                }
+      const numRows = this.rompecabezas.length;
+      const numCols = this.rompecabezas[0].length;
+      let valorEsperado = 1;
+  
+      for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+          if (this.rompecabezas[i][j].textContent !== "") {
+            const valorActual = parseInt(this.rompecabezas[i][j].textContent);
+            if (valorActual !== valorEsperado) {
+              return false;
             }
+            valorEsperado++;
+          }
         }
-        return true;
+      }
+      return true;
     }
-}
-
-const juego = new Juego()
+  }
+  
+  const juego = new Juego();
+  
