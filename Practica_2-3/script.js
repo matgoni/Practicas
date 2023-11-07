d6397e2ca7cfae3d532f0ccc147ab941db8f5f0f
Matgoni <matgoni19@gmail.com>
2023-11-06 21:40:37 -0600

    Quiza un buen inicio jsjsjs

---
+++ Practica_2-3/script.js
@@ -0,0 +1,36 @@
+const searchInput = document.querySelector("input[name='search']");
+const filterSelect = document.querySelector("select[name='filter']");
+const applyButton = document.querySelector("input[value='Aplicar']");
+const filterButton = document.querySelector("input[value='Filtrar']");
+const tableBody = document.querySelector("tbody");
+
+searchInput.addEventListener("input", () => {
+
+});
+
+// Filtrar por género al presionar el botón "Filtrar"
+filterButton.addEventListener("click", () => {
+
+});
+
+// Eliminar registros al presionar el botón "Aplicar" después de seleccionar "Eliminar"
+applyButton.addEventListener("click", () => {
+
+});
+
+// Función para filtrar por título
+const filterByTitle = (term) => {
+
+};
+
+// Función para filtrar por género
+const filterByGenre = (genre) => {
+
+};
+
+// Función para eliminar registros seleccionados
+const deleteSelectedRecords = () => {
+
+};
+  
+  
