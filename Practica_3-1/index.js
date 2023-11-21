a096c2194abb51bad3ce4bb8ba4ad994860df4b4
Israeljuarezjimenez <144080402+Israeljuarezjimenez@users.noreply.github.com>
2023-11-21 11:45:29 -0600

    posible finalización de práctica

--- Practica_3-1/index.js
+++ Practica_3-1/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const path = require('path'); // Importar el módulo path
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+// Configurar las rutas del proyecto
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/formulario', (req, res) => {
+  res.sendFile(path.join(__dirname, 'formulario.html'));
+});
+
+// Otros posibles endpoints...
+
+// Configurar la escucha del servidor
 app.listen(PORT, () => {
-  console.log(`Servidor en el puerto ${PORT}`);
+  console.log(`Escuchando en el puerto ${PORT}`);
 });
