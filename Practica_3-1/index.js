3293defdf4d009eed5843ca761c093a1cd716e28
Matgoni <matgoni19@gmail.com>
2023-11-21 14:16:14 -0600

    reajuste

--- Practica_3-1/index.js
+++ Practica_3-1/index.js
@@ -1,21 +1,17 @@
 const express = require('express');
-const path = require('path'); // Importar el módulo path
+const path = require('path'); 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-// Configurar las rutas del proyecto
-app.get('/', (req, res) => {
+app.get('/', (res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/formulario', (req, res) => {
+app.get('/formulario', (res) => {
   res.sendFile(path.join(__dirname, 'formulario.html'));
 });
 
-// Otros posibles endpoints...
-
-// Configurar la escucha del servidor
 app.listen(PORT, () => {
   console.log(`Escuchando en el puerto ${PORT}`);
 });
