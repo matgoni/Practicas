024c8f700052726bb50d82024df44fe47982b4a2
Matgoni <matgoni19@gmail.com>
2023-11-21 21:58:19 -0600

    agregando callbacks explicitos

--- Practica_3-1/index.js
+++ Practica_3-1/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+function enviarArchivo(res, ruta) {
+  res.sendFile(path.join(__dirname, ruta), (error) => {
+    if (error) {
+      console.error(`Error al enviar el archivo ${ruta}: ${error.message}`);
+      res.status(500).send('Error interno del servidor');
+    } else {
+      console.log(`Archivo ${ruta} enviado correctamente`);
+    }
+  });
+}
 
-app.get('/', (res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', (req, res) => {
+  enviarArchivo(res, 'index.html');
 });
 
-app.get('/formulario', (res) => {
-  res.sendFile(path.join(__dirname, 'formulario.html'));
+app.get('/formulario', (req, res) => {
+  enviarArchivo(res, 'formulario.html');
 });
 
 app.listen(PORT, () => {
