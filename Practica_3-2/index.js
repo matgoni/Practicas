5c86a32c50e6db63e39c00a17889309a77df92a1
matgoni <matgoni19@gmail.com>
2023-11-27 23:55:56 -0600

    Ya quedo el index y se esta elaborando la base de dtos

--- Practica_3-2/index.js
+++ Practica_3-2/index.js
@@ -1,27 +1,48 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import express from express;
+import cors from 'cors';
+import SpaceRoute from './scripts/api/SpaceRoute.js';
+import SpaceDAO from './scripts/api/SpaceDAO.js';
+import dotenv from 'dotenv';
+import mongodb from 'mongodb';
 
-const PORT = process.env.PORT || 8000;
-function enviarArchivo(res, ruta) {
-  res.sendFile(path.join(__dirname, ruta), (error) => {
-    if (error) {
-      console.error(`Error al enviar el archivo ${ruta}: ${error.message}`);
-      res.status(500).send('Error interno del servidor');
-    } else {
-      console.log(`Archivo ${ruta} enviado correctamente`);
-    }
-  });
+class Index {
+	
+	static app = express();
+	static router = express.Router();
+	
+	static main() {
+		dotenv.config();
+		Index.setUpServer();
+		Index.setUpDatabase();
+	}
+
+	static setUpServer() {
+		Index.app.use(cors());
+		Index.app.use(express.json());
+		Index.app.use(express.urlencoded({ extended: true }));
+		Index.app.use('*', SpaceRoute.configRoutes(Index.router));
+		Index.app.use('*', (req, res) => {
+			res.status(404).json({ error: 'not found' });
+		});
+	}
+	
+	static async setUpDatabase() {
+
+		const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
+		const port = process.env.PORT || 8000;
+		console.log(port);
+		try {
+			// Connect to the MongoDB cluster
+			await client.connect();
+			await SpaceDAO.injectDB(client);
+			Index.app.listen(port, () => {
+				console.log(`server is running on port:${port}`);
+			});
+		} catch (e) {
+			console.error(e);
+			process.exit(1);
+		}
+	}
 }
 
-app.get('/', (req, res) => {
-  enviarArchivo(res, 'index.html');
-});
-
-app.get('/formulario', (req, res) => {
-  enviarArchivo(res, 'formulario.html');
-});
-
-app.listen(PORT, () => {
-  console.log(`Escuchando en el puerto ${PORT}`);
-});
+Index.main();
